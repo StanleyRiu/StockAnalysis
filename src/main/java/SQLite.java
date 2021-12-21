1c91a296f97e1fa27f467c6302c4ef0dbea44e47
DESKTOP-LE4HIM7\Stanley Riu <rkriu@cht.com.tw>
2021-12-21 16:31:48 +0800

    m

--- src/main/java/SQLite.java
+++ src/main/java/SQLite.java
@@ -1,14 +1,27 @@
 import java.sql.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class SQLite {
     String db_name = "stock.db";
-    String Statement_of_Comprehensive_Income;   //綜合損益表
+    Map<String, String> Statement_of_Comprehensive_Income = null;   //綜合損益表
     String Statement_of_Financial_Position;     //資產負債表
     String Statement_of_Cash_Flows;             //現金流量表
     String Statement_of_Operating_Profit;       //營益分析查詢彙總表
     Connection connection = null;
 
     public SQLite() {
+        Statement_of_Comprehensive_Income = Map.ofEntries(
+                Map.entry("year", "integer"),
+                Map.entry("season", "integer"),
+                Map.entry("type", "string"),
+                Map.entry("id", "string"),
+                Map.entry("name", "string"),
+                Map.entry("eps", "float")
+        );
+
         // create a database connection
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:"+db_name);
@@ -22,16 +35,16 @@ public class SQLite {
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-            statement.executeUpdate("drop table if exists "+table);
-            statement.executeUpdate("create table person (id string, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
-            ResultSet rs = statement.executeQuery("select * from person");
+            statement.executeUpdate("drop table if exists " + table);
+//            statement.executeUpdate("create table person (id string, name string)");
+//            statement.executeUpdate("insert into person values(1, 'leo')");
+//            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from "+table);
             while(rs.next())
             {
                 // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
+//                System.out.println("name = " + rs.getString("name"));
+//                System.out.println("id = " + rs.getInt("id"));
             }
         } catch(SQLException e) {
             // if the error message is "out of memory",
